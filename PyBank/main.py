bf1dfd8167a4850fbf36618afc059e972af0242c
Crihan13na <crihan.anastasia@gmail.com>
2019-12-21 14:04:56 -0600

    final revision

---
+++ PyBank/main.py
@@ -0,0 +1,83 @@
+#import library
+import os
+import csv
+
+#showing the path
+budget_data = os.path.join ("budget_data.csv")
+
+# open and read csv
+with open( budget_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    # skip header row
+    print(f"Header: {csv_header}")
+
+    # find net amount of profit and loss
+    total_month = []
+    revenue = []
+    revenue_change = []
+    monthly_change = []
+    
+
+    #read through each row of data after header
+    #Calculates the total number of Months       
+    for row in csvreader:
+        total_month.append(row[0])
+        revenue.append(row[1])
+    
+ #Calculates the total Revenue 
+    revenue_int = map(int,revenue)
+    total_revenue = (sum(revenue_int))
+    
+
+ #Calculates the Change of Avarage
+    i = 0
+    for i in range(len(revenue) - 1):
+        profit_loss = int(revenue[i+1]) - int(revenue[i])
+ 
+        revenue_change.append(profit_loss)
+    Total = sum(revenue_change)
+    #print(revenue_change)
+    monthly_change = Total / len(revenue_change)
+    print(monthly_change)
+    #print(Total)
+    
+#Calculates the Greatest Increase
+    greatest_increase = max(revenue_change)
+    
+    greatIn = revenue_change.index(greatest_increase)
+    total_month_increase = total_month[greatIn+1]
+    
+#Calculates the Greatest Decrease
+    greatest_decrease = min(revenue_change)
+    
+    greatD = revenue_change.index(greatest_decrease)
+    total_month_decrease = total_month[greatD+1]
+
+
+#Print Statements
+print("                                 ")
+print('Financial Analysis')
+print('----------------------------')
+
+
+print("Total number of months: " + str(len(total_month)))
+
+print("Total Revenue in period: $ " + str(total_revenue))
+      
+print("Average monthly change in Revenue : $" + str(monthly_change))
+
+print("Greatest Increase in Profits:  " + str(total_month_increase) + " ($" + str(greatest_increase)+ ") " )
+
+print("Greatest Decrease in Profits: " + str(total_month_decrease) + " ($" + str(greatest_decrease) + ") ")
+print("--------------------------------")
+
+#write file
+f = open("pybank.txt","w")   
+#add file in write mode
+f.write("Total number of months: " + str(len(total_month)))
+f.write("Total Revenue in period: $ " + str(total_revenue))
+f.write("Average monthly change in Revenue : $" + str(monthly_change))
+f.write(f"Greatest Increase in Profits: " + str(total_month_increase) + " ($" +  str(greatest_increase)+ ") " )
+f.write(f"Greatest Decrease in Profits: " + str(total_month_decrease) + " ($" + str(greatest_decrease)+ ") " )
+f.close
---
+++ PyPoll/main.py
@@ -0,0 +1,71 @@
+import os
+import csv
+
+pypoll_csv = os.path.join( "election_data.csv")
+
+#set lists to store data
+candidate = []
+
+#with open to read election_data.csv file
+with open(pypoll_csv) as csvfile:
+  csvreader = csv.reader(csvfile, delimiter=',')
+
+  reader=next(csvreader)
+
+# Read each row of data after the header
+  for row in csvreader:
+      
+#add candidate
+    candidate.append(row[2])
+
+
+#Calculates the percentage of votes for each candidate
+Khan = candidate.count("Khan")
+Khan_percent = (int(Khan) / len(candidate) * 100)
+
+Correy = candidate.count("Correy")
+Correy_percent = (int(Correy) / len(candidate) * 100)
+
+
+Li = candidate.count("Li")
+Li_percent = (int(Li) / len(candidate) * 100)
+
+
+OTooley = candidate.count("O'Tooley")
+OTooley_percent = (int(OTooley) / len(candidate) * 100)
+
+
+#Comparing the number of votes
+if Khan_percent >= .50:
+	print("Winner: Khan")
+elif Correy_percent >= .50:
+	print("Winner: Correy")
+elif Li_percent >= .50:
+	print("Winner: Li")
+else:
+	print("Winner: O'Tooley")	
+
+# print total for each candidate, wtih percentage and total votes
+# print election header
+print("                             ")
+print("----------------------------")
+print("Election Results")
+print("----------------------------")
+print("Total Votes: " + str(len(candidate)))
+print("----------------------------")
+print("Khan: " + str((round(Khan_percent))) + "% (" + str(Khan) + ")")
+print("Correy: " + str((round(Correy_percent))) + "% (" + str(Correy) + ")")
+print("Li: " + str((round(Li_percent))) + "% (" + str(Li) + ")")
+print("O'Tooley " + str((round(OTooley_percent))) + "% (" + str(OTooley) + ")")
+
+print("----------------------------")
+
+
+# write file
+f = open("pypoll.txt","w")   
+# add file in write mode
+f.write("Khan: " + str((round(Khan_percent))) + "% (" + str(Khan) + ")")
+f.write("Correy: " + str((round(Correy_percent))) + "% (" + str(Correy) + ")")
+f.write("Li: " + str((round(Li_percent))) + "% (" + str(Li) + ")")
+f.write("O'Tooley " + str((round(OTooley_percent))) + "% (" + str(OTooley) + ")")
+f.close()		
